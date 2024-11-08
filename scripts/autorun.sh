a82f055c88e24e63b2111b4699380420a4e2c8f1
Orkun Gürler <orkun.guerler@gmail.com>
2024-11-08 21:01:43 +0100

    add applications to autorun

--- scripts/autorun.sh
+++ scripts/autorun.sh
@@ -1,1 +1,14 @@
 # Autorun script for window managers
+#!/bin/sh
+
+run () {
+    if ! pgrep -f "$1";
+    then
+        "$@" &
+    if
+}
+
+run "picom"
+run "volumeicon"
+run "dunst"
+run "feh" --randomize --recursive --bg-fill ~/Pictures/Wallpapers/
